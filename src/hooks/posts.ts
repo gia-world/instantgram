9c1b77a312bf516975044209547b1876e0ff1a5b
Gia <gia.jihye.j@gmail.com>
2024-01-12 18:29:11 +0900

    Update: 주석 추가

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -11,6 +11,9 @@ export default function usePost() {
       body: JSON.stringify({ id: post.id, like }),
     }).then(() => mutate("/api/posts"));
     // mutate(key) : key를 사용하는 모든 데이터의 캐시가 revalidate 됨
+
+    // 캐싱이 필요한 데이터, GET 요청은 SWR
+    // 캐싱하면 안되는 동적 데이터, PUT, DELETE, PATCH 등 수정 요청은 fetch/axios
   };
   return { posts, isLoading, error, setLike };
 }
