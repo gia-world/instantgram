3948a29eae3daa267d19a25d28658a28bd0afc9c
Gia <gia.jihye.j@gmail.com>
2024-01-15 00:38:25 +0900

    Feat: follow

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -2,6 +2,7 @@
 
 import useMe from "@/hooks/me";
 import { ProfileUser } from "@/model/user";
+// import useSWR from "swr";
 import Button from "./ui/Button";
 
 type Props = {
@@ -10,16 +11,26 @@ type Props = {
 export default function FollowButton({ user }: Props) {
   const { username } = user;
   // 로그인 여부, 팔로잉 여부에 따라 달라짐
-  const { user: LoggedInUser } = useMe();
-  const showButton = LoggedInUser && LoggedInUser.username !== username;
+  const { user: LoggedInUser, toggleFollow } = useMe();
+
+  // const { data: follow, isLoading, error, mutate } = useSWR("/api/follow");
+  // console.log(follow, "follow");
+  const loggedInUsername = LoggedInUser && LoggedInUser.username;
+
+  const showButton = loggedInUsername !== username;
   const following =
     LoggedInUser &&
     LoggedInUser.following.find((item) => item.username === username);
   const text = following ? "Unfollow" : "Follow";
+
+  const handleFollow = () => {
+    toggleFollow(user.id, !following);
+  };
+
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={() => {}} red={text === "Unfollow"} />
+        <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />
       )}
     </>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Button({ text, onClick, red }: Props) {
   return (
     <button
-      onClick={onClick}
+      onClick={() => onClick()}
       className={`rounded-md border-none px-8 py-2 font-bold leading-4 text-white ${
         red ? "bg-red-500" : "bg-sky-500"
       }`}
