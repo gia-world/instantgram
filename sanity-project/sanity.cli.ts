40e0f39c5e5cf2e92a3b33d4d122e4c014e6db33
Gia <gia.jihye.j@gmail.com>
2024-01-15 21:57:35 +0900

    Update: vercel ignore

--- sanity-project/sanity.cli.ts
+++ sanity-project/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'lvanct6q',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
